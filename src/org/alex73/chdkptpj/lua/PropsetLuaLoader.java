14b82c8863cdce9b3668d67a7d7b1ab15080a4f1
Aleś Bułojčyk <alex73mail@gmail.com>
2015-06-29 10:15:56 +0300

    Propsets handling

---
+++ src/org/alex73/chdkptpj/lua/PropsetLuaLoader.java
@@ -0,0 +1,43 @@
+/**************************************************************************
+ chdkptpJ - Java CHDK PTP framework.
+
+ Copyright (C) 2015 Aleś Bułojčyk (alex73mail@gmail.com)
+
+ This file is part of chdkptpJ.
+
+ chdkptpJ is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ chdkptpJ is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **************************************************************************/
+package org.alex73.chdkptpj.lua;
+
+import org.luaj.vm2.Globals;
+import org.luaj.vm2.LoadState;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.compiler.LuaC;
+import org.luaj.vm2.lib.jse.JsePlatform;
+
+/**
+ * Loader for propseN.lua files.
+ */
+public class PropsetLuaLoader {
+
+    public static LuaValue load(String file) throws Exception {
+        // initialize Lua standard packages
+        Globals globals = JsePlatform.standardGlobals();
+
+        LoadState.install(globals);
+        LuaC.install(globals);
+
+        return globals.loadfile(file).call();
+    }
+}
